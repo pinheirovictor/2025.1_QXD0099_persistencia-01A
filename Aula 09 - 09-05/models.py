159133c2ae005ec1815d65cffb38f12139636f93
Francisco Victor da Silva Pinheiro <37351953+pinheirovictor@users.noreply.github.com>
2025-11-27 18:40:18 -0300

    Remove commented-out model classes and relationships
    Removed commented-out code for Aluno, Curso, and Inscricao classes.

--- Aula 09 - 09-05/models.py
+++ Aula 09 - 09-05/models.py
@@ -49,40 +49,6 @@ class Inscricao(Base):
     curso = relationship('Curso', back_populates='inscricoes')
 
 
-# from sqlalchemy.orm import relationship, declarative_base 
-# from sqlalchemy import Column, Integer, String, ForeignKey
-
-# Base = declarative_base()
-
-# class Aluno(Base):
-#     __tablename__ = 'alunos'
-#     id = Column(Integer, primary_key=True)
-#     nome = Column(String, nullable=False)
-#     email = Column(String, unique=True, nullable=False)
-    
-#     inscricoes = relationship('Inscricao', back_populates='aluno')
-    
-
-# class Curso(Base):
-#     __tablename__ = 'cursos'
-#     id = Column(Integer, primary_key=True)
-#     nome = Column(String, nullable=False)
-#     descricao = Column(String)
-    
-#     inscricoes = relationship('Inscricao', back_populates='curso')
-        
-        
-# class Inscricao(Base):
-#     __tablename__ = 'inscricoes'
-    
-#     id = Column(Integer, primary_key=True)
-#     aluno_id = Column(Integer, ForeignKey('alunos.id'))
-#     curso_id = Column(Integer, ForeignKey('cursos.id'))
-    
-#     aluno = relationship('Aluno', back_populates='inscricoes')
-#     curso = relationship('Curso', back_populates='inscricoes')
-    
-        
         
         
         
