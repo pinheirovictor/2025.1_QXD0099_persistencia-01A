bfa9df4cbb558f0106c051fc269fb66417eaf717
Francisco Victor da Silva Pinheiro <37351953+pinheirovictor@users.noreply.github.com>
2025-11-27 18:40:41 -0300

    Update database.py

--- Aula 09 - 09-05/database.py
+++ Aula 09 - 09-05/database.py
@@ -31,26 +31,3 @@ def get_db():
         db.close()  # Garante que a sessão será fechada após o uso
 
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from models import Base
-
-# DATABASE_URL = "sqlite:///./escola.db"
-
-# # Criar o motor do banco de dados
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-# # Criar as tabelas
-# Base.metadata.create_all(bind=engine)
-
-# # Configuração da Sessão
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# # Dependência para obter a sessão
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
